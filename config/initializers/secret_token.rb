dcdad02f8328b7715000f1408ac3d849a216d73b
Chu Anh Tuan <chu.anh.tuan@framgia.com>
2014-04-11 17:44:43 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fels1::Application.config.secret_key_base = 'cac1826fceb24e0338ee80a32d584010c1d1c2756b7d5aa7971329650f0d4a7edda57ced8263b3474ebffcea481dd55daadb8e63bbf00797b3c7dbf9b4e43b6c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fels1::Application.config.session_store :cookie_store, key: '_fels_1_session'
